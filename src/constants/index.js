7419b7ebbd8a02125f7c925e97d4d4abea34527c
rozer <shubhamsinha07084@gmail.com>
2024-06-14 15:48:46 +0530

    new project

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navItems = [
+  {
+    label: "Home",
+    href: "#",
+  },
+  {
+    label: "Features",
+    href: "#",
+  },
+  {
+    label: "Workflow",
+    href: "#",
+  },
+  {
+    label: "Pricing",
+    href: "#",
+  },
+  {
+    label: "Testimonials",
+    href: "#",
+  },
+];
